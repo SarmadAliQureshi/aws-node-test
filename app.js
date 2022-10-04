51b28c514b6103d4783434b1a62c3acb202814a6
SarmadAliQureshi <sarmadalikaharian@gmail.com>
2022-10-04 11:35:03 +0500

    make it better

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express')
 require("dotenv").config();
+var {Pool} = require('pg');
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'postgres',
+    port: 5432,
+  })
 
 app = express()
 
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res, next) {
-    res.render('index')
+    pool.query('select site_name from "ajk plantation 2020_21" limit 1')
+    .then(function(results) { 
+        console.log('results:', results.rows);
+        res.render('index',{data:results.rows[0]})
+    })
+    
 })
 
 app.listen(process.env.PORT, function() {
